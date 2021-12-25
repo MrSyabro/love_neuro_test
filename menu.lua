a4b407302e146b947007ca610478e3898f48e1b3
MrSyabro <syabro1998@gmail.com>
2021-12-25 15:35:10 +0200

    Дабвил grid в меню

--- menu.lua
+++ menu.lua
@@ -15,12 +15,18 @@ local function convert_color(color)
     return c
 end
 
-color1 = ui.new_textentry("color1_entry", utils.tc(0.01, 0.02))
+grid = ui.new_grid ("grid", 0, 0, utils.tc(1, 1))
+
+color1 = ui.new_textentry("color1_entry")
 color1.textinput = "#bf616a"
-color2 = ui.new_textentry("color2_entry", utils.tc(0.01, 0.1))
+color2 = ui.new_textentry("color2_entry")
 color2.textinput = "#ebcb8b"
-color3 = ui.new_textentry("color3_entry", utils.tc(0.01, 0.18))
+color3 = ui.new_textentry("color3_entry")
 color3.textinput = "#b48ead"
+
+text1 = ui.new_text("color1_text")
+text2 = ui.new_text("color2_text")
+text3 = ui.new_text("color3_text")
 
 button1 = ui.new_button("start_bt", utils.tc(0.01, 0.26))
 button1.printed_name = "Start"
@@ -35,7 +41,15 @@ function button1.callbacks.mousepressed(self)
     engine:load_scene("neuro_scene.lua")
 end
 
-objs:add(color1)
-objs:add(color2)
-objs:add(color3)
-objs:add(button1)
+grid1 = ui.new_grid ("grid1")
+grid:add(grid1, 2, 2)
+
+grid1:add(text1, 1, 1)
+grid1:add(text2, 1, 2)
+grid1:add(text3, 1, 3)
+grid1:add(color1, 2, 1)
+grid1:add(color2, 2, 2)
+grid1:add(color3, 2, 3)
+grid1:add(button1, 3, 3)
+
+objs:add(grid1)
